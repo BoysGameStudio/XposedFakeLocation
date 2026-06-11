ce10b5f296b5287463205cf6573cda604ee0b40f
noobexon1 <noamf92@gmail.com>
2026-06-11 06:21:00 +0300

    refactor(location): remove outdated TODO comment in LocationApiHooks
    - Eliminated a TODO comment regarding scope checks in hooks to improve code clarity and reduce clutter.
    - Streamlined the hookLocation method by removing unnecessary whitespace, enhancing overall readability.

--- app/src/main/java/com/noobexon/xposedfakelocation/xposed/hooks/LocationApiHooks.kt
+++ app/src/main/java/com/noobexon/xposedfakelocation/xposed/hooks/LocationApiHooks.kt
@@ -6,8 +6,6 @@ import com.noobexon.xposedfakelocation.xposed.utils.LocationUtil
 import com.noobexon.xposedfakelocation.xposed.utils.PreferencesUtil
 import io.github.libxposed.api.XposedInterface
 
-// TODO: in all hooks, we should check every 3 seconds if we are still in scope. isPlaying is not enough.
-
 class LocationApiHooks(private val module: XposedInterface, private val classLoader: ClassLoader) {
     private val tag = "[LocationApiHooks]"
 
@@ -19,7 +17,6 @@ class LocationApiHooks(private val module: XposedInterface, private val classLoa
     private fun hookLocation() {
         runCatching {
             val locationClass = Class.forName("android.location.Location", false, classLoader)
-
             with(locationClass) {
                 hookMethod("getLatitude", enabled = { true }) { LocationUtil.latitude }
                 hookMethod("getLongitude", enabled = { true }) { LocationUtil.longitude }
